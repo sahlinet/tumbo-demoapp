9d67881f3cd4cb0a3c51d935cfbf8fb34a8de1ca
Philip Sahli <philip@sahli.net>
2018-07-29 21:57:53 +0200

    update function

--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,6 @@
 def func(self):
+	from core import responses
+	import json
 	data = {"message": "Hello people!"}
 	self.datastore.write_dict(data)
-	return "data written"
+	return responses.JSONResponse(json.dumps({"message": "Data written to datastore"}))
